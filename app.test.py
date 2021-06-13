d1d79fb84851e0853ffd71c1b9cf46ca73c48b0a
parzival867 <nathyadiah@gmail.com>
2021-06-13 15:45:35 -0400

    Add database code and code for registration

--- app.test.py
+++ app.test.py
@@ -16,5 +16,9 @@ def test_other(self):
 	response = tester.get('test', content_type='html/text')
 	self.assertEqual(response.status_code, 404)
 
+def test_database(self):
+	tester = os.path.exists("flaskr.db")
+	self.assertTrue(tester)
+
 if __name__ == '__main__':
 	unittest.main()
