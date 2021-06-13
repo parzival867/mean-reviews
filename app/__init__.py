096320967178c4a489cef758fad37d991f17f4d4
parzival867 <nathyadiah@gmail.com>
2021-06-13 16:10:00 -0400

    Restructure to use app factory, add __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+# __init__.py
+
+import os
+from flask import Flask
+
+def create_app(config_file=None):
+	# create and configure the app
+	app = Flask(__name__)
+
+	# configuration
+	if (config_name is not None):
+		app.config.from_pyfile(config_file, silent=True)
+	else:
+		app.config.from_mapping(
+			SECRET_KEY='dev',
+			DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
+		)
+
+	# attach routes/ views and database functions
+
+	return app
